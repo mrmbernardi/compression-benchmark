2c7f2e23987baa94301fd930086fbc39c8eb5cde
Michael Bernardi <michael.bernardi@ifta.com>
2024-02-19 17:19:29 +0100

    updates cmethods for snappy

--- notebook/cmethods.py
+++ notebook/cmethods.py
@@ -82,8 +82,10 @@ class Method:
 BSC = Method("Bsc (lossless)", ["float", "double"])
 LFZIP_BSC = Method("LfZip with Bsc", ["float", "double"])
 LFZIP_LZ4 = Method("LfZip with Lz4", ["float", "double"])
+LFZIP_SNAPPY = Method("LfZip with Snappy", ["float", "double"])
 LFZIP_STREAMSPLITV_BSC = Method("LfZip with Stream Split (V) with Bsc", ["float", "double"])
 LFZIP_STREAMSPLITV_LZ4 = Method("LfZip with Stream Split (V) with Lz4", ["float", "double"])
+LFZIP_STREAMSPLITV_SNAPPY = Method("LfZip with Stream Split (V) with Snappy", ["float", "double"])
 LFZIP_STREAMSPLITV_ZSTD3 = Method("LfZip with Stream Split (V) with Zstd (3)", ["float", "double"])
 LFZIP_ZSTD3 = Method("LfZip with Zstd (3)", ["float", "double"])
 LZ4 = Method("Lz4 (lossless)", ["float", "double"])
@@ -91,25 +93,33 @@ MACHETE = Method("Machete", ["double"])
 MASK_BSC = Method("Mask with Bsc", ["float", "double"])
 MASK_LZ4 = Method("Mask with Lz4", ["float", "double"])
 MASK_PCODEC = Method("Mask with Pcodec", ["float", "double"])
+MASK_SNAPPY = Method("Mask with Snappy", ["float", "double"])
 MASK_STREAMSPLIT4_BSC = Method("Mask with Stream Split (4) with Bsc", ["float"])
 MASK_STREAMSPLIT4_LZ4 = Method("Mask with Stream Split (4) with Lz4", ["float"])
+MASK_STREAMSPLIT4_SNAPPY = Method("Mask with Stream Split (4) with Snappy", ["float"])
 MASK_STREAMSPLIT4_ZSTD3 = Method("Mask with Stream Split (4) with Zstd (3)", ["float"])
 MASK_STREAMSPLIT8_BSC = Method("Mask with Stream Split (8) with Bsc", ["double"])
 MASK_STREAMSPLIT8_LZ4 = Method("Mask with Stream Split (8) with Lz4", ["double"])
+MASK_STREAMSPLIT8_SNAPPY = Method("Mask with Stream Split (8) with Snappy", ["double"])
 MASK_STREAMSPLIT8_ZSTD3 = Method("Mask with Stream Split (8) with Zstd (3)", ["double"])
 MASK_ZSTD3 = Method("Mask with Zstd (3)", ["float", "double"])
 PCODEC = Method("Pcodec (lossless)", ["float", "double"])
 QUANTISE_BSC = Method("Quantise with Bsc", ["float", "double"])
 QUANTISE_LZ4 = Method("Quantise with Lz4", ["float", "double"])
+QUANTISE_SNAPPY = Method("Quantise with Snappy", ["float", "double"])
 QUANTISE_STREAMSPLITV_BSC = Method("Quantise with Stream Split (V) with Bsc", ["float", "double"])
 QUANTISE_STREAMSPLITV_LZ4 = Method("Quantise with Stream Split (V) with Lz4", ["float", "double"])
+QUANTISE_STREAMSPLITV_SNAPPY = Method("Quantise with Stream Split (V) with Snappy", ["float", "double"])
 QUANTISE_STREAMSPLITV_ZSTD3 = Method("Quantise with Stream Split (V) with Zstd (3)", ["float", "double"])
 QUANTISE_ZSTD3 = Method("Quantise with Zstd (3)", ["float", "double"])
+SNAPPY = Method("Snappy (lossless)", ["float", "double"])
 STREAMSPLIT4_BSC = Method("Stream Split (4) with Bsc (lossless)", ["float"])
 STREAMSPLIT4_LZ4 = Method("Stream Split (4) with Lz4 (lossless)", ["float"])
+STREAMSPLIT4_SNAPPY = Method("Stream Split (4) with Snappy (lossless)", ["float"])
 STREAMSPLIT4_ZSTD3 = Method("Stream Split (4) with Zstd (3) (lossless)", ["float"])
 STREAMSPLIT8_BSC = Method("Stream Split (8) with Bsc (lossless)", ["double"])
 STREAMSPLIT8_LZ4 = Method("Stream Split (8) with Lz4 (lossless)", ["double"])
+STREAMSPLIT8_SNAPPY = Method("Stream Split (8) with Snappy (lossless)", ["double"])
 STREAMSPLIT8_ZSTD3 = Method("Stream Split (8) with Zstd (3) (lossless)", ["double"])
 SZ3 = Method("Sz3", ["float", "double"])
 ZSTD3 = Method("Zstd (3) (lossless)", ["float", "double"])
@@ -118,8 +128,10 @@ AllMethods = [
     BSC,
     LFZIP_BSC,
     LFZIP_LZ4,
+    LFZIP_SNAPPY,
     LFZIP_STREAMSPLITV_BSC,
     LFZIP_STREAMSPLITV_LZ4,
+    LFZIP_STREAMSPLITV_SNAPPY,
     LFZIP_STREAMSPLITV_ZSTD3,
     LFZIP_ZSTD3,
     LZ4,
@@ -127,25 +139,33 @@ AllMethods = [
     MASK_BSC,
     MASK_LZ4,
     MASK_PCODEC,
+    MASK_SNAPPY,
     MASK_STREAMSPLIT4_BSC,
     MASK_STREAMSPLIT4_LZ4,
+    MASK_STREAMSPLIT4_SNAPPY,
     MASK_STREAMSPLIT4_ZSTD3,
     MASK_STREAMSPLIT8_BSC,
     MASK_STREAMSPLIT8_LZ4,
+    MASK_STREAMSPLIT8_SNAPPY,
     MASK_STREAMSPLIT8_ZSTD3,
     MASK_ZSTD3,
     PCODEC,
     QUANTISE_BSC,
     QUANTISE_LZ4,
+    QUANTISE_SNAPPY,
     QUANTISE_STREAMSPLITV_BSC,
     QUANTISE_STREAMSPLITV_LZ4,
+    QUANTISE_STREAMSPLITV_SNAPPY,
     QUANTISE_STREAMSPLITV_ZSTD3,
     QUANTISE_ZSTD3,
+    SNAPPY,
     STREAMSPLIT4_BSC,
     STREAMSPLIT4_LZ4,
+    STREAMSPLIT4_SNAPPY,
     STREAMSPLIT4_ZSTD3,
     STREAMSPLIT8_BSC,
     STREAMSPLIT8_LZ4,
+    STREAMSPLIT8_SNAPPY,
     STREAMSPLIT8_ZSTD3,
     SZ3,
     ZSTD3,
